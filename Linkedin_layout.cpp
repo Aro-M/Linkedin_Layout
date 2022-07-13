d9e85b1597daf966a3450e3600dc37d57756f633
Aro MIlitosyan <ar.militosyan.97@mail.ru>
2022-07-13 17:23:01 +0400

    Update Linkedin_layout.cpp

---
+++ Linkedin_layout.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+
+
+class  Linkedin_Hom_page{
+public:
+friend class  Join_now;
+friend class  Sign_in;
+friend class  Search_for_job;
+friend class  Find_a_person_job;
+friend class  Learn_a_new_skill;
+friend class  Content_topics;  // mej eli ban ka 
+friend class  Suggested_searches;  //mej el ban ka 
+friend class  Post_job;
+friend class  Learn_skill;
+friend class  General; //mejy  ban ka  
+friend class  Browse_Linkedin;  // mejy ban ka
+friend class  Business_Solutions;
+friend class Directories;
+friend class Down_page; // mej ban ka eli
+}
+
+
+
+class Join_now{
+public:
+void input_function(){std::string email;
+std::stirng password;
+std::cout<< "Email" << std:: endl;
+std::cin >> email;
+std::string email;
+std::cout<< "Password(6   or more  characters)" << std:: endl;
+std::cin >> password;
+if(email =  email1 && password == password1){
+void registrant_person_data(){}
+}else{
+std::cout <<"Something is wrong,try again...";
+}
+}
+std::cout << "By clicking Agree & Join, you agree to the LinkedIn User Agreement, Privacy Policy, and Cookie Policy."<<std::endl;
+void User_agreement_button();
+void Privacy_Policy_button();
+void Cookie_Policy_button();
+void  button_Agree_And_join();
+std::cout << "Already on Linkedin";
+void  Join_now_button_Sign_in();
+std::cout << "Looking to create a page for a business? GetHelp " <<std::endl;
+void  Join_now_button_get_help();
+
+};
+
+
+class  Sign_in{
+public:
+std::cout << LinkedIn" << std::endl;
+std::cout << "Welcome Back
+Don't miss your next opportunity. Sign 
+in to stay updated on your professional world."<< std::endl;
+void my_page_home(){};
+};
+
+class Content_topics{ 
+void See_all_topics();
+void Remote();
+void Work_from_Home();
+void Retirement();
+void Internships();
+void Freelancer();
+void Salary_and_compensation();
+void starting_a_job();
+
+};
+
+
+class   Suggested_searches{
+public:
+void Engineering();
+void Business_Development();
+void Finance();
+void Administractive_Assistant();
+void Retail_associate();
+void Customer_Service();
+void Operations();
+void   Information_technology();
+void Marketing();
+void Human Resources();
+void  Button_show_more();
+
+};
+
+class General{
+public:
+void sign_up();
+void Help_center();
+void About();
+void Press();
+void Blog();
+void Careers();
+void Developers();
+
+};
+
+class Browse_linkedin{
+public:
+void  learing();
+void  Jobs();
+void  Salary();
+void  Mobile();
+void Services();
+void Products();
+
+};
+
+class Business_solutions{
+public:
+void Talent();
+void Marketing():
+void Sales();
+void Learning ();
+
+};
+
+class  Directories{
+public:
+void Members();
+void Jobs();
+void Companies();
+void Featured();
+void Learning();
+void Posts();
+void Articles();
+void Schools();
+void News();
+void News_letters();
+void Services();
+void Products();
+void content__topics();
+
+};
+
